bb4149d7c1474d9bcde957a7d0d2d8d162d77c91
arnav <arnavagarwal@urbancompany.com>
2022-11-23 00:14:42 +0530

    experiment with animated value

--- App.tsx
+++ App.tsx
@@ -13,29 +13,37 @@ import { Animated, Text, TouchableOpacity, View} from 'react-native';
 
 const App = () => {
 
-  const value = useState(new Animated.ValueXY({x:0, y:0}))[0]
+  const leftValue = useState(new Animated.Value(0))[0]
 
   function moveBall() {
-    Animated.timing(value, {
-      toValue: {x:100, y:100},
+    Animated.timing(leftValue, {
+      toValue: 100,
       duration: 1000,
       useNativeDriver: false
     }).start()
   }
   
   return (
-    <View>
-      <Animated.View style={value.getLayout()}>
-        <View style={{
-          width: 100,
-          height: 100,
-          borderRadius: 100/2,
-          backgroundColor: 'red'
-        }}/>
-      </Animated.View>
-      <TouchableOpacity onPress={moveBall}>
-        <Text>Hey Click me!!!</Text>
+    <View
+      style={{flex: 1}}>
+      <View style ={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Animated.View
+          style={{
+            width: 100,
+            height: 100,
+            borderRadius: 100/2,
+            marginLeft: leftValue,
+            backgroundColor: 'red',
+        }}
+        />
+        <TouchableOpacity onPress={moveBall}>
+          <Text> Move me</Text>
         </TouchableOpacity>
+      </View>
     </View>
   );
 };
